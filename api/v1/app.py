c5a01dd4599c45a926123d6aee5f637a6b11f560
Daniel E Rodriguez <dr2d4@live.com>
2020-01-29 22:11:26 -0500

    Add docstrings in files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    Create app flask
+"""
 from flask import Flask, make_response, jsonify
 
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    Create routes for status api, ...
+"""
 from api.v1.views import app_views
 
 from flask import jsonify
