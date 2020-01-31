95d9b3b251559621ce0eeabe0d56acfe34c5aba3
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-30 20:08:21 -0500

    [Feature] new view for Places with POST

---
+++ api/v1/views/places.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Places Views """
+from flask import jsonify, abort, request
+
+from api.v1.views import app_views
+
+from models.place import Place
+
+from models import storage
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
+def create_place(city_id):
+    """
+        Create new Place
+    """
+    if not storage.get('City', city_id):
+        abort(404)
+    if not storage.get('User', user_id):
+        abort(404)
+    r_json = request.get_json()
+
+    if not r_json:
+        abort(400, "Not a JSON")
+    elif not r_json.get('user_id'):
+        abort(400, 'Missing user_id')
+    elif not r_json.get('name'):
+        abort(400, 'Missing name')
+
+    nobj = Place(city_id=city_id, **r_json)
+    resp = nobj.to_dict()
+    nobj.save()
+
+    return jsonify(resp), 201
